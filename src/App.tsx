f415d196483fb6c8a5f59f890cec78cbe83c8b90
MetroHege <113589548+MetroHege@users.noreply.github.com>
2025-02-10 10:37:02 +0200

    add snow material files and update Box positions in App component

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,8 @@ const App = () => {
       <Canvas>
         <ambientLight />
         <pointLight position={[10, 10, 10]} />
-        <Box position={[-1, 0, 0]} />
-        <Box position={[1, 0, 0]} />
+        <Box position={[-2, 0, 0]} />
+        <Box position={[2, 0, 0]} />
       </Canvas>
     </section>
   );
